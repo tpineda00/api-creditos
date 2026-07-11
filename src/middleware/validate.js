41f61efcf1d594f11d78f00ea7b36a03dbb2858b
Jose Alejandro Ferrera Chacon <jaferrerac@unah.hn>
2026-07-11 09:52:03 -0600

    Modificar los archivos solicitudes.js , validate.js,solicitud.schemas.js para que sean compatibles con type:modules

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,11 +1,12 @@
 //Middleware  de Validacion  funcion Higher-Order Function
-export const validateSchema = (schema) => {
+ export const validate = (schema) => {
 
     return (req, res, next) =>{
         const resultado = schema.safeParse(req.body);
         if(!resultado.sucess){
         return res.status(400).json({
             status: "Error",
+            message: "Validacion de datos fallida",
             errors: resultado.error.errors.map(err =>({
                 campo: err.path.join('.'),
                 mensaje: err.message
